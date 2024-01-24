83ef61b8ffafa8c012e78e89d4ca9fafdbce2934
sajidrahman700 <sr.rahman700@gmail.com>
2024-01-24 23:52:41 +0600

    edit on testAdvance

--- src/test/java/first/com/WebAutomationBasic/TestAdvanceWebElement.java
+++ src/test/java/first/com/WebAutomationBasic/TestAdvanceWebElement.java
@@ -7,14 +7,17 @@ import org.testng.annotations.Test;
 public class TestAdvanceWebElement extends DriverSetup {
 	@Test
 	public void TestElement() throws InterruptedException {
-		browser.get("https://www.ebl.com.bd/onlineapply");
-		WebElement branch = browser.findElement(By.xpath("//select[@name='form_branch']"));
-	    branch.click();
-		Thread.sleep(5000);
+	
+		browser.get("https://www.ebl.com.bd/retail/EBL-Cards");
 		
-		WebElement terms = browser.findElement(By.cssSelector("input[class='required']"));
-	    terms.click();
-		Thread.sleep(5000);
-	}
+		//Locate direct child throw their parent.Here we want to locate h2.
+		System.out.println(browser.findElement(By.xpath("//section/div/div/div/h2")).getText());  //parent/child
+		System.out.println(browser.findElement(By.cssSelector("section>div>div>div>h2")).getText());  //parent>child
+		
+		//Locate element using Multiple Attribute
+		System.out.println(browser.findElement(By.xpath("//a[@class='cta-call' and @href='tel:+8809612316230']")).getText()); // //tagname[@attribute='value' and @attribute='value']
+		System.out.println(browser.findElement(By.cssSelector("a[class='cta-call'][href='tel:+8809612316230']")).getText());  // tagname[attribute='value'][@attribute='value']
+		
 
 }
+}	
